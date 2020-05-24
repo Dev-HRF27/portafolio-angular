128f0e3681225c362505ec213f7cb633f3d53366
Heriberto Ruiz <beto65.ruizf@gmail.com>
2020-05-24 01:02:53 -0500

    Fin seccion 4

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: any = {};
+  cargada = false;
+
+  constructor(private http: HttpClient) {
+
+      console.log("servicio listo");
+
+      // Leer el archivo Json
+      this.http.get('assets/data/data-pagina.json')
+      .subscribe( resp => {
+        this.cargada = true;
+        this.info = resp;
+        console.log(resp);
+
+      })
+
+  }
+}
