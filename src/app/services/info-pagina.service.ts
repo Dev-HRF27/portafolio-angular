b3e6425fefa7b399cedddff0b70dcf1b881c0607
Heriberto Ruiz <beto65.ruizf@gmail.com>
2020-05-24 20:25:27 -0500

    Fin de la sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { InfoPagina } from "../interfaces/info-pagina.interface";
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
-
-  info: any = {};
+  info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
-      console.log("servicio listo");
+  private cargarInfo(){
+    // Leer el archivo Json
+    this.http.get('assets/data/data-pagina.json')
+    .subscribe( (resp: InfoPagina) => {
+      this.cargada = true;
+      this.info = resp;
+    })
+  }
 
-      // Leer el archivo Json
-      this.http.get('assets/data/data-pagina.json')
-      .subscribe( resp => {
-        this.cargada = true;
-        this.info = resp;
-        console.log(resp);
+  private cargarEquipo(){
+    // Leer el archivo Json
+    this.http.get('https://angular-html-efd41.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
 
-      })
-
+      this.equipo = resp;
+      //console.log(resp);
+    })
   }
 }
